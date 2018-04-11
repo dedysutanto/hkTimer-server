4fdb57cd43e39cc03f75d747520ae8380d8b53e7
Dedy Sutanto <dedy.sutanto@proit.co.id>
2018-04-11 18:25:48 +0700

    default is admin

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('proadmin/', admin.site.urls),
+    path('', admin.site.urls),
     path('', include('product.urls')),
 ]
